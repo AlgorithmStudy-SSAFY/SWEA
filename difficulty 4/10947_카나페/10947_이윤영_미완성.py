4f64126f3cd550fa20fe2f102139682f65e73558
YunyLee <happydbsdud@gmail.com>
2021-10-25 22:11:43 +0900

    swea문제

---
+++ difficulty 4/10947_카나페/10947_이윤영_미완성.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('input_1012.txt', 'r')
+
+def find_cabbage():
+    global cnt
+
+    for i in range(N+2):
+        for j in range(M+2):
+            if arr[i][j] and not arr[i-1][j] and not arr[i][j-1]:
+                cnt += 1
+
+TC = int(input())
+for test_case in range(1, TC+1):
+    N, M, K = map(int,input().split())
+    arr = [[0]*M for _ in range(N)]
+    for m in range(K):
+        a, b = map(int,input().split())
+        arr[a][b] = 1
+
+    # 우선 좌표에 맞게 1표시를 다 해준 후, 갑옷을 입힌다.
+    for lst in arr:
+        lst.insert(0, 0)
+        lst.append(0)
+    arr.append([0]*(M+2))
+    arr.insert(0, [0]*(M+2))
+    # 갑옷 두르기 완성
+
+    cnt = 0
+    find_cabbage()
+    print(cnt)
+
+
